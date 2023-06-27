434005457e9bc55e832aab38f15b73e8cd33477c
vicenteneto7 <pvicenteneto7@gmail.com>
2023-06-27 09:07:14 -0300

    fix: correção do erro no clientName

---
+++ index.js
@@ -0,0 +1,62 @@
+const { response } = require('express')
+const express = require('express')
+const uuid = require('uuid')
+const port = 3000
+const app = express()
+
+app.use(express.json())
+
+const orders = []
+
+
+const checkUserId = (request, response, next) => {
+    const { id } = request.params
+    const index = orders.findIndex(order => order.id === id)
+
+    if (index < 0) {
+        return response.status(404).json({ error: "user not found" })
+    }
+
+    request.userIndex = index
+    request.userId = id
+
+    next()
+
+}
+
+app.get('/order', (request, response) => {
+    return response.status(201).json(orders)
+
+})
+
+
+app.post('/order', (request, response) => {
+    const { order, clientName, price } = request.body
+    const orderPlaced = { id: uuid.v4(), order, clientName, price, status: "Em preparação" }
+
+    orders.push(orderPlaced)
+
+    return response.status(201).json(orderPlaced)
+
+})
+
+app.put('/order/:id', checkUserId, (request, response) => {
+    const index = request.userIndex
+    const id = request.userId
+    const { clientName ,order, price } = request.body
+
+    const orderUpdate = { id, order, clientName, price, status: "Em preparação" }
+
+    orders[index] = orderUpdate
+
+
+    return response.status(201).json(orderUpdate)
+
+})
+
+
+
+
+app.listen(port, () => {
+    console.log(`server started on ${port}`)
+})
